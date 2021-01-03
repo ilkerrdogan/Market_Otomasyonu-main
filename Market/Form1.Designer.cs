20d8b192f16fd0ce8fecc9999f48fa88e947227d
İlker DOĞAN <ilkerdogan012327@gmail.com>
2021-01-03 17:49:08 +0300

    Form1 değişikliliği

--- Market/Form1.Designer.cs
+++ Market/Form1.Designer.cs
@@ -72,7 +72,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(126, 253);
+            this.button1.Location = new System.Drawing.Point(125, 252);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 59);
             this.button1.TabIndex = 0;
